c08727f8c26c6d762aaabf5691b14aaf1da25007
AngelaMTC <tc.angela.m@gmail.com>
2022-12-05 22:55:51 -0600

    fix: Bugs fixed

--- components/Categories/Categories.js
+++ components/Categories/Categories.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text } from 'react-native';
 
-export const Categories = () => {
+export default function Categories () {
   return (
     <View>
       <Text>Categorias</Text>
     <TouchableOpacity>
-        <View style={styles.fab}>
-            <text>jkjjkl</text>
+        <View style={style.fab}>
+            <Text>jkjjkl</Text>
         </View>
     </TouchableOpacity>
     </View>
